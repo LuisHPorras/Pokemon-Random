3801a723bcbd52f7533afc573d32f90911020038
Luis H. Porras <luishercep@gmail.com>
2016-06-10 12:39:25 +0200

    Union2

--- Unified_Pokemon_Random/Adventure/Main.cpp
+++ Unified_Pokemon_Random/Adventure/Main.cpp
@@ -9,6 +9,8 @@
 #include "Player_Trainer.h"
 #include "Map.h"
 
+#include "Fight.h"
+
 //using namespace std;
 
 //The window we'll be rendering to
@@ -25,92 +27,106 @@ int previousKey;
 bool quit;
 
 Mundo mundo;
+Fight fight;
 
 int main(int argc, char* args[])
 {
 	
 
 	//Start up SDL and create window
-	if (!mundo.inicializaSdl())
+	if (!mundo.inicializaSdl() && !fight.init())
 	{
 		printf("Failed to initialize!\n");
 	}
 	else
 	{
-		//Main loop flag
-		bool quit = false;
-
-		//Event handler
-		SDL_Event e;
-		
-		//Movable_Trainer *trainers[5];
-		Player_Trainer player;
-
-		Map route_0;
-
-		/*Texture background;
-
-		background.loadFromFile("Zona_Safari.png");*/
-
-		//While application is running
-		while (!quit)
+		if (!fight.loadMedia())
 		{
-			SDL_RenderClear(gRenderer);
-			route_0.printMap();
-			//background.render((SCREEN_WIDTH - background.getWidth()) / 2, (SCREEN_HEIGHT - background.getHeight()) / 2);
-			
-			//Handle events on queue
-			if (SDL_PollEvent(&e) != 0)
+			cout << "Failed to load media!" << endl;
+		}
+		else
+		{
+
+			//Main loop flag
+			bool quit = false;
+
+			//Event handler
+			SDL_Event e;
+		
+			//Movable_Trainer *trainers[5];
+			Player_Trainer player;
+
+			Map route_0;
+
+			/*Texture background;
+
+			background.loadFromFile("Zona_Safari.png");*/
+
+			//While application is running
+			while (!quit)
 			{
-				//User requests quit
-				if (e.type == SDL_QUIT)
+				SDL_RenderClear(gRenderer);
+				route_0.printMap();
+				//background.render((SCREEN_WIDTH - background.getWidth()) / 2, (SCREEN_HEIGHT - background.getHeight()) / 2);
+
+				//Handle events on queue
+				if (SDL_PollEvent(&e) != 0)
 				{
-					quit = true;
-				}
-				else if( e.type == SDL_KEYDOWN )
-				{
-					//Select surfaces based on key press
-					switch( e.key.keysym.sym )
+					//User requests quit
+					if (e.type == SDL_QUIT)
 					{
+						quit = true;
+					}
+					else if (e.type == SDL_KEYDOWN)
+					{
+						//Select surfaces based on key press
+						switch (e.key.keysym.sym)
+						{
+						case SDLK_k:
+							fight.events(e, quit);
+							fight.render();
+							break;
 						case SDLK_UP:
-						player.move(Constants::KEY_PRESS_DIRECTION_UP);
-						//trainers[0]->move(KEY_PRESS_PLAYER_UP);
-						previousKey = Constants::KEY_PRESS_DIRECTION_UP;
-						break;
+							player.move(Constants::KEY_PRESS_DIRECTION_UP);
+							//trainers[0]->move(KEY_PRESS_PLAYER_UP);
+							previousKey = Constants::KEY_PRESS_DIRECTION_UP;
+							break;
 
 						case SDLK_DOWN:
-						player.move(Constants::KEY_PRESS_DIRECTION_DOWN);
-						//trainers[0]->move(KEY_PRESS_PLAYER_DOWN);
-						previousKey = Constants::KEY_PRESS_DIRECTION_DOWN;
-						break;
+							player.move(Constants::KEY_PRESS_DIRECTION_DOWN);
+							//trainers[0]->move(KEY_PRESS_PLAYER_DOWN);
+							previousKey = Constants::KEY_PRESS_DIRECTION_DOWN;
+							break;
 
 						case SDLK_LEFT:
-						player.move(Constants::KEY_PRESS_DIRECTION_LEFT);
-						//trainers[0]->move(KEY_PRESS_PLAYER_LEFT);
-						previousKey = Constants::KEY_PRESS_DIRECTION_LEFT;
-						break;
+							player.move(Constants::KEY_PRESS_DIRECTION_LEFT);
+							//trainers[0]->move(KEY_PRESS_PLAYER_LEFT);
+							previousKey = Constants::KEY_PRESS_DIRECTION_LEFT;
+							break;
 
 						case SDLK_RIGHT:
-						player.move(Constants::KEY_PRESS_DIRECTION_RIGHT);
-						//trainers[0]->move(KEY_PRESS_PLAYER_RIGHT);
-						previousKey = Constants::KEY_PRESS_DIRECTION_RIGHT;
-						break;
+							player.move(Constants::KEY_PRESS_DIRECTION_RIGHT);
+							//trainers[0]->move(KEY_PRESS_PLAYER_RIGHT);
+							previousKey = Constants::KEY_PRESS_DIRECTION_RIGHT;
+							break;
 
 						default:
-						break;
+							break;
+						}
 					}
 				}
-			}
-			player.animate();
-			//trainers[0]->animate();
+				player.animate();
+				//trainers[0]->animate();
 
-			//Update screen
-			SDL_RenderPresent(gRenderer);
+				//Update screen
+				SDL_RenderPresent(gRenderer);
+			}
 		}
 	}
 
 	//Free resources and close SDL
 	mundo.close();
+	fight.close();
 
 	return 0;
 }
